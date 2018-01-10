a47eb34e3060be9ad0941217017885b78e314b25
Dipali Biradar <dipalibi@cybage.com>
2018-01-10 20:15:35 +0530

    done

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-component',
+  // Component As : CSS selector : Syntax //selector: 'app-server-component'//
+  // Component As : Attribute selector : Syntax //selector: '[app-server-component]'//
+  // Component As : css class selector : Syntax //selector: '.app-server-component'//
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+}
