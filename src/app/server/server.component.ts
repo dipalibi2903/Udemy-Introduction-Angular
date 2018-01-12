9fcd7b428f982970d3b3ba1546fe7869905328f8
Dipali Biradar <dipalibi@cybage.com>
2018-01-12 17:55:09 +0530

    today

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,4 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  serverId:number= 10;
+  serverStatus:string= 'Offline For timebing';
+  allowServerButton=true;
+  serverNameEnterted='Test Server';
+  username='';
+
+  constructor(){
+    setTimeout(()=> {
+      this.allowServerButton=false;
+    }, 2000);
+  }
+
+  getStatusOfServer(){
+    return this.serverStatus;
+  }
+
+  onSeverCreate(){
+    this.serverStatus='Server has been created succesfully!!! with the name'+ this.serverNameEnterted;
+  }
+
+
+  onUpdateSeverName(event:Event){
+    this.serverNameEnterted=(<HTMLInputElement>event.target).value;
+  }
+
+
+  onResetIt(){
+    this.username='';
+  }
 }
